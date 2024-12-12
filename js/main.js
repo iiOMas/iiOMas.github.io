915c808418be34de9b893eaf581e3fc2771190e1
ShahdMahmoud9 <shahdmahmoud161@gmail.com>
2024-12-13 00:45:12 +0200

    2nd update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// menu side ber
+
 function showSideBar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex'
@@ -10,3 +12,15 @@ function hideSideBar() {
     const menu = document.querySelector('.menu-icon');
     menu.style.display='flex';
 }
+
+// end menu side bar
+
+// header bgcolor changing
+document.addEventListener('scroll' , () => {
+    const header = document.querySelector('header');
+    if(window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+})
