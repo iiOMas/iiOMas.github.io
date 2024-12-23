2bd98de71f56aeecdb1f51f68a0605cbd84e1449
Sherry-Medhat <sherrymedhatmoris@gmail.com>
2024-12-23 10:26:50 +0200

    p6

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 function showSideBar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex'
-    // const menu = document.querySelector('.menu-icon');
-    // menu.style.display='none';
+    const menu = document.querySelector('.menu-icon');
+    menu.style.display='none';
 }
 function hideSideBar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none'
-    // const menu = document.querySelector('.menu-icon');
-    // menu.style.display='flex';
+    const menu = document.querySelector('.menu-icon');
+    menu.style.display='flex';
 }
 
 // end menu side bar
